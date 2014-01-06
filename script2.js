5e60e95713d53c658cad0375b0da927a9fc63605
dianamin <97.diana@gmail.com>
2014-01-06 21:13:09 +0200

    Random

--- script2.js
+++ script2.js
@@ -1,20 +1,37 @@
 var n = 6;
-var width = 3;
+var width = 6;
 var height = 2;
 var ids = new Array ();
+var found = new Array ();
+
+
+var random = function () {
+	var value = Math.floor (n*Math.random ()) + 1;
+	while (found [value] == 2) {
+		value = Math.floor (n*Math.random ()) + 1;
+	}
+	found[value]++;
+	return value;
+}
 
 
 $(document).ready (function () {
-	$('.card').css ('background-color', 'red');
 
+	for (var i = 0; i < n; i++) found[i] = 0;
 	for (var i = 0; i < height; i++) {
 		$('table').append ("<tr> ");
 		for (var j = 0; j < width; j++) {
-			console.log (j);
-			ids [i*3+j] = String (i*3 + j);
-			$('tr').append ("<td> <div class = \"card\">" + String (i*3+j+1) + "</div> </td>");
+			var value = random ();
+			ids [i*3+j] = String (value-1);
+			$('tr:last').append ("<td> <div class = \"card\">" + String (value) + "</div> </td>");
 		}
 		$('table').append ("</tr>");
 	}
+
+	$('.card').css ('background-color', 'darkred');
+
+	$('.card').on ('click', function () {
+		$('.card').css ('background-color', 'white');
+	});
 
 })
